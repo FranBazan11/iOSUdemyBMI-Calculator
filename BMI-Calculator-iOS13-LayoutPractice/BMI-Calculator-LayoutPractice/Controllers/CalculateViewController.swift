ac557842aaf64768dfa29a46a86ee7cdbce3f32f
Francisco Bazán <juanfrancisco.bazancarrizo@mercadolibre.com>
2020-12-17 12:56:32 -0300

    Add app BMI-Calculator

---
+++ BMI-Calculator-iOS13-LayoutPractice/BMI-Calculator-LayoutPractice/Controllers/CalculateViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  BMI-Calculator-LayoutPractice
+//
+//  Created by Angela Yu on 21/08/2019.
+//  Copyright © 2019 App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class CalculateViewController: UIViewController {
+    
+   var calculatorBrain = CalculatorBrain()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightSlider: UISlider!
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    
+    @IBAction func heightSliderChanged(_ sender: UISlider) {
+        let height = String(format: "%.2f", sender.value)
+        heightLabel.text = "\(height)m"
+    }
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
+        let weight = String(Int(sender.value))
+        weightLabel.text = "\(weight)Kg"
+    }
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        let height = heightSlider.value
+        let weight = weightSlider.value
+        
+        calculatorBrain.calculateBMI(height: height, weight: weight )
+        
+        self.performSegue(withIdentifier: "goToResult", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "goToResult" {
+            let destinationVC = segue.destination as! ResultViewController
+            destinationVC.bmiValue = calculatorBrain.getValueBMI()
+            destinationVC.advice = calculatorBrain.getAdvice()
+            destinationVC.color = calculatorBrain.getColor()
+        }
+    }
+}
+
---
+++ BMI-Calculator-iOS13-LayoutPractice/BMI-Calculator-LayoutPractice/Models/BMI.swift
@@ -0,0 +1,15 @@
+//
+//  BMI.swift
+//  BMI-Calculator-LayoutPractice
+//
+//  Created by francisco bazan on 4/4/20.
+//  Copyright © 2020 App Brewery. All rights reserved.
+//
+
+import UIKit
+
+struct BMI {
+    var value: Float
+    let advice: String
+    let color: UIColor
+}
